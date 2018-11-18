0e19d729ca98bda0360932c012b06bb1d0f7c77e
Jakub Zarzycki <jakub.zarzycki_xl@wp.pl>
2018-11-18 17:36:20 +0100

    first tests passed

--- wpf/interval_tree/iSet.ml
+++ wpf/interval_tree/iSet.ml
@@ -1,24 +1,6 @@
 (*
  * Written by: Jakub Zarzycki 371722
  * Review    : Alicja Ziarko
- * Based on:
- * PSet - Polymorphic sets
- * Copyright (C) 1996-2003 Xavier Leroy, Nicolas Cannasse, Markus Mottl
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version,
- * with the special exception on linking described in file LICENSE.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
 
 
@@ -31,33 +13,9 @@
  * initially i wanted to just create pSet with 'k = 'a * 'a but it wouldn't work
  * T_T
  *)
-type 'a set =
-  | Empty
-  | Node of 'a set * 'a * 'a set * int
-
 type 'a tree =
-  {
-    cmp : 'a -> 'a -> int;
-    set : 'a set;
-  }
-
-
-(* 
- * cmp introduces partial order on intervals.
- * disjoint intervals are well-ordered and intersecting intervals arehandled
- * via exceptions
- *)
-(* we assume that 2nd pair (x, y) came from our set *)
-let cmp (a, b) (x, y) =
-  if b < x then -1
-  else if y < a then 1
-  else if a = x && b = y then 0 
-  else if a > x && b < y then raise Contained
-  else raise Nonemptyintersection
-;;
-
-
-type t = (int * int) tree;;
+  | Empty
+  | Node of 'a tree * 'a * 'a tree * int
 
 (* excteption to handle non empty intersections of intervals in cmp, 
  * but one is not contained in the other*)
@@ -65,6 +23,26 @@ exception Nonemptyintersection;;
 
 (* exception for handling one interval contained in the other *)
 exception Contained;;
+
+(* 
+ * cmp introduces partial order on intervals.
+ * disjoint intervals are well-ordered and intersecting intervals are handled
+ * via exceptions
+ *)
+(* we assume that 2nd pair (x, y) came from our set *)
+let cmp (a, b) (x, y) =
+  if (b + 1) < x then -1
+  else if y < (a - 1) then 1
+  else if a = x && b = y then 0 
+  else if a >= x && b <= y then raise Contained
+  else raise Nonemptyintersection
+;;
+
+let in_interval x (a, b) =
+  a <= x && x <= b
+;;
+
+type t = (int * int) tree;;
 
 let height = function
   | Node (_, _, _, h) -> h
@@ -122,28 +100,28 @@ let merge t1 t2 =
       bal t1 k (remove_min_elt t2)
 ;;
 
-let create cmp = 
-  { cmp = cmp; set = Empty }
+let create = 
+  Empty
 ;;
 
 let empty = 
-  { cmp = compare; set = Empty }
+  Empty
 ;;
 
 let is_empty x = 
-  x.set = Empty
+  x = Empty
 ;;
 
-let rec add_one cmp x = function
+let rec add x = function
   | Node (l, k, r, h) ->
       (try
         let c = cmp x k in
         if c = 0 then Node (l, x, r, h)
         else if c < 0 then
-          let nl = add_one cmp x l in
+          let nl = add x l in
           bal nl k r
         else
-          let nr = add_one cmp x r in
+          let nr = add x r in
           bal l k nr
       with
         | Contained ->
@@ -157,50 +135,56 @@ let rec add_one cmp x = function
   | Empty -> Node (Empty, x, Empty, 1)
 ;;
 
-let add x { cmp = cmp; set = set } =
-  { cmp = cmp; set = add_one cmp x set }
-;;
-
 (*i give it 50-50, we'll see in tests*)
-let rec join cmp l v r =
+let rec join l v r =
   match (l, r) with
-  | (Empty, _) -> add_one cmp v r
-  | (_, Empty) -> add_one cmp v l
+  | (Empty, _) -> add v r
+  | (_, Empty) -> add v l
   | (Node(ll, lv, lr, lh), Node(rl, rv, rr, rh)) ->
-      if lh > rh + 2 then bal ll lv (join cmp lr v r) else
-      if rh > lh + 2 then bal (join cmp l v rl) rv rr else
+      if lh > rh + 2 then bal ll lv (join lr v r) else
+      if rh > lh + 2 then bal (join l v rl) rv rr else
       make l v r
 ;;
 
-let split x { cmp = cmp; set = set } =
-  let x = (x, x) in
+let split x set =
   let rec loop x = function
     | Empty ->
         (Empty, false, Empty)
-    | Node (l, v, r, _) ->
+    | Node (l, (lower, upper), r, _) ->
       (try
-        let c = cmp x v in
-        if c = 0 then 
-          let lower, high = v in 
-            (join cmp l (lower, (fst x)) Empty), 
-            true, 
-            (join cmp Empty ((fst x), high) r)
+        let c = cmp (x, x) (lower, upper) in
+        if c = 0 then  
+          (join l (lower, x - 1) Empty), 
+          true, 
+          (join Empty (x + 1, upper) r)
         else if c < 0 then
-          let (ll, pres, rl) = loop x l in (ll, pres, join cmp rl v r)
+          let (ll, pres, rl) = loop x l in 
+            (ll, pres, join rl (lower, upper) r)
         else
-          let (lr, pres, rr) = loop x r in (join cmp l v lr, pres, rr)
+          let (lr, pres, rr) = loop x r in 
+            (join l (lower, upper) lr, pres, rr)
       with
-        | Contained | Nonemptyintersection ->
-            let lower, high = v in 
-              (join cmp l (lower, (fst x)) Empty), 
-              true, 
-              (join cmp Empty ((fst x), high) r))
+        | Contained ->
+            (join l (lower, x - 1) Empty), 
+            true, 
+            (join Empty (x + 1, upper) r)
+        | Nonemptyintersection -> 
+            if in_interval x (lower, upper) 
+            then
+              (join l (lower, x) Empty), 
+              true,
+              (join Empty (x, upper) r)
+            else
+              if x < lower then
+                (l, false, join Empty (lower, upper) r)
+              else 
+                (join l (lower, upper) Empty, false, r))
   in
   let setl, pres, setr = loop x set in
-  { cmp = cmp; set = setl }, pres, { cmp = cmp; set = setr }
+    setl, pres, setr
 ;;
 
-let remove (x, y) { cmp = cmp; set = set } =
+let remove (x, y) set =
   let rec loop = function
     | Node (l, (lower, upper), r, _) ->
         (try 
@@ -211,39 +195,33 @@ let remove (x, y) { cmp = cmp; set = set } =
         with 
           | Nonemptyintersection ->
               let k = if lower < x then (lower, x) else (y, upper) in
-              join cmp l k r
+              join l k r
           | Contained ->
               merge
                 (merge l (Node (Empty, (lower, x), Empty, 1)))
                 (merge r (Node (Empty, (y, upper), Empty, 1))))
     | Empty -> Empty in
-  { cmp = cmp; set = loop set }
+        loop set
 ;;
 
-let mem (x : int) ({ cmp = cmp; set = set } : t) =
+let mem x set =
   let rec loop = function
-    | Node (l, k, r, _) ->
-        (try 
-            let c = cmp (x, x) k in
-            c = 0 || loop (if c < 0 then l else r)
-        with
-            (* if interval has non-empty intersection with any interval
-               in the set, but is not equal then it cannot be a memeber *)
-            Nonemptyintersection -> false)
+    | Node (l, (a, b), r, _) ->
+        in_interval x (a, b) || if x < a then loop l else loop r
     | Empty -> false in
   loop set
 ;;
 
 let exists = mem;;
 
-let iter f { set = set } =
+let iter f set =
   let rec loop = function
     | Empty -> ()
     | Node (l, k, r, _) -> loop l; f k; loop r in
   loop set
 ;;
 
-let fold f { cmp = cmp; set = set } acc =
+let fold f set acc =
   let rec loop acc = function
     | Empty -> acc
     | Node (l, k, r, _) ->
@@ -251,15 +229,19 @@ let fold f { cmp = cmp; set = set } acc =
   loop acc set
 ;;
 
-let elements { set = set } = 
+let elements set = 
   let rec loop acc = function
-      Empty -> acc
-    | Node(l, k, r, _) -> loop (k :: loop acc r) l in
+    | Empty -> acc
+    | Node (l, k, r, _) -> loop (k :: loop acc r) l in
   loop [] set
 ;;
 
 let below x s =
-  let (leq, _, _) = split x s in 
-  List.length (elements leq)
+  let rec loop acc = function
+    | Empty -> acc
+    | Node (l, (a, b), r, _) -> acc + (b - a + 1) + loop acc l + loop acc r
+  in
+    let (leq, _, _) = split x s in
+    1 + loop 0 leq
 ;;
 
